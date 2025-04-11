20a1b9b4d796ed0fad7f7f1a1ab4802c37d44902
Anna-Hax <dhruvi.purohit06@gmail.com>
2025-04-11 12:51:18 +0530

    cors

--- backend/cors/index.js
+++ backend/cors/index.js
@@ -1,6 +1,6 @@
 import cors from 'cors'
 const corsOptions = {
-    origin: ['http://127.0.0.1:5500', 'https://calendarjs-qc65.onrender.com'],
+    origin: ['http://127.0.0.1:5500', 'https://glittering-douhua-b8e0a1.netlify.app'],
     optionsSuccessStatus: 200,
 };
  
